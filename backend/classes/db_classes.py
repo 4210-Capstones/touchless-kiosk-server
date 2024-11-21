9522acfe841aedd4d84dcfad8a216951d137c534
kingomes <omaressa13@aol.com>
2024-11-21 14:13:32 -0600

    added and adjusted tables and diagrams
    Co-authored-by: MominAsif and leilaglewis

--- backend/classes/db_classes.py
+++ backend/classes/db_classes.py
@@ -2,7 +2,6 @@
 All database classes will be declared here
 """
 from datetime import datetime
-
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import declared_attr, relationship
 
@@ -47,10 +46,12 @@ class User(DBParentClass):
     __abstract__ = False
 
     user_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
-    email = Column(String(100), unique=True, nullable=False)
-    password = Column(String(255), nullable=False)   # store hashed, not plain
+    user_email = Column(String(100), unique=True, nullable=False)
+    user_first = Column(String(100), nullable=False)
+    user_last = Column(String(100), nullable=False)
+    user_password = Column(String(255), nullable=False)   # store hashed, not plain
 
-    user_roles = relationship("UserRole", back_populates="users")
+    user_roles = relationship("Role", secondary="userrole", back_populates="users")
 
 class Role(DBParentClass):
     __abstract__ = False
@@ -59,59 +60,106 @@ class Role(DBParentClass):
     role_name = Column(String(50), nullable=False)
     description = Column(String(255), nullable=False)
 
+    users = relationship("User", secondary="userrole", back_populates="user_roles")
+
 class UserRole(DBParentClass):
     __abstract__ = False
 
     user_id = Column(Integer, ForeignKey(User.user_id), primary_key=True)
     role_id = Column(Integer, ForeignKey(Role.role_id), primary_key=True)
 
-    users = relationship("User", back_populates="user_roles")
-
-class Service(DBParentClass):
+class Tag(DBParentClass):
     __abstract__ = False
 
-    service_roomnumber = Column(Integer, primary_key=True, nullable=False, unique=False)
-    service_name = Column(String(50), nullable=False)
+    tag_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    tag_name = Column(String(50), nullable=False)
+    tag_description = Column(String, nullable=True)
 
-class Booking(DBParentClass):
-    __abstract__ = False
-
-    booking_startdate = Column(DateTime, primary_key=True, nullable=False, unique=False)
-    booking_enddate = Column(DateTime, nullable=False, unique=False)
-    booking_userid = Column(Integer, ForeignKey(User.user_id), primary_key=True, nullable=False, unique=True)
-    booking_name = Column(String(50), nullable=False)
-    booking_email = Column(String(100), unique=True, nullable=False)
-    booking_roomnumber = Column(Integer, ForeignKey(Service.service_roomnumber), primary_key=True, unique=False)
+    images = relationship("Image", secondary="imagetag", back_populates="tags")
 
 class Image(DBParentClass):
     __abstract__ = False
 
-    image_link = Column(String, primary_key=True, nullable=False)
+    image_link = Column(String, primary_key=True, unique=True, nullable=False)
+
+    image_requests = relationship("ImageRequest", back_populates="images")
+    tags = relationship("Tag", secondary="imagetag", back_populates="images")
+
+class ImageTag(DBParentClass):
+    __abstract__ = False
+
+    image_link = Column(String, ForeignKey(Image.image_link), primary_key=True)
+    tag_id = Column(Integer, ForeignKey(Tag.tag_id), primary_key=True)
+
+class Room(DBParentClass):
+    __abstract__ = False
+
+    room_number = Column(Integer, primary_key=True, nullable=False, unique=True)
+    room_available = Column(Boolean, nullable=False, unique=False)
+
+    bookings = relationship("Booking", secondary="bookingroom", back_populates="rooms")
+    
+class BookingType(DBParentClass):
+    __abstract__ = False
+
+    bookingtype_id = Column(Integer, primary_key=True, nullable=False, unique=False)
+    bookingtype_name = Column(String(50), nullable=False)
+    bookingtype_description = Column(String(255), nullable=False)
+    
+class Booking(DBParentClass):
+    __abstract__ = False
+
+    booking_id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    bookingtype_id = Column(Integer, ForeignKey(BookingType.bookingtype_id), unique=False)
+    booking_startdate = Column(DateTime, nullable=False, unique=False)
+    booking_enddate = Column(DateTime, nullable=False, unique=False)
+    booking_userid = Column(Integer, ForeignKey(User.user_id), nullable=False, unique=True)
+
+    rooms = relationship("Room", secondary="BookingRoom", back_populates="bookings")
+
+class BookingRoom(DBParentClass):
+    __abstract__ = False
+
+    bookingroom_id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    booking_id = Column(Integer, ForeignKey(Booking.booking_id))
+    room_number = Column(Integer, ForeignKey(Room.room_number))
+
+
+class ImageRequest(DBParentClass):
+    __abstract__ = False
+
+    imgreq_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    imgreq_name = Column(String(50), nullable=False)
+    imgreq_email = Column(String(100), unique=True, nullable=False)
+    imgreq_message = Column(String, nullable=False)
+    imgreq_link = Column(String, ForeignKey(Image.image_link), nullable=False)
+    imgreq_startdate = Column(DateTime, nullable=False, unique=False)
+    imgreq_enddate = Column(DateTime, nullable=False, unique=False)
+
+    images = relationship("Image", back_populates="image_requests")
+
 
 class Club(DBParentClass):
     __abstract__ = False
 
     club_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    club_name = Column(String, nullable=False, unique=True)
+    club_name = Column(String(50), nullable=False, unique=True)
     club_description = Column(String, nullable=True)
     club_upcoming_activities = Column(String, nullable=True)
 
-    requests = relationship("ClubRequest", back_populates="club")
+    requests = relationship("ClubRequest", back_populates="clubs")
 
 class ClubRequest(DBParentClass):
     __abstract__ = False
 
     clubreq_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    clubreq_name = Column(String, nullable=False)
-    clubreq_email = Column(String, nullable=False)
+    clubreq_name = Column(String(50), nullable=False)
+    clubreq_email = Column(String(100), nullable=False)
     clubreq_date = Column(DateTime, default=datetime.utcnow())
     club_id = Column(Integer, ForeignKey(Club.club_id), nullable=False)
 
-    club = relationship("Club", back_populates="requests")
+    clubs = relationship("Club", back_populates="requests")
 
-# class Tutoring(DBParentClass):
-    
-    
 # # Inspect and list tables
 # def list_tables():
 #     inspector = inspect(ENGINE)  # ENGINE should be the SQLAlchemy engine instance
--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,9 @@
+from classes.db_classes import User, Booking, Service, Club, ClubRequest, Image, Role, UserRole
+from database.Service import UserService
+from database.config_db import create_db
+from database.dependency_db import get_db
+from database.config_db import engine
+
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -5,7 +11,6 @@ from fastapi import FastAPI
 from api.LoginAPI import login_router
 from api.UserAPI import user_router
 from database import config_db
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -22,4 +27,37 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(user_router)
-app.include_router(login_router)
+
+# just a test for db so far
+if __name__ == '__main__':
+
+    create_db()
+
+    # Image.__table__.drop(engine)
+    # ClubRequest.__table__.drop(engine)
+    # Club.__table__.drop(engine)
+    # Booking.__table__.drop(engine)
+    # Service.__table__.drop(engine)
+    # UserRole.__table__.drop(engine)
+    # Role.__table__.drop(engine)
+    # User.__table__.drop(engine)
+
+    user = User(email='<EMAIL>', password='<PASSWORD>')
+
+    db = next(get_db())
+    user_id = UserService.create(user, db).user_id
+    print(f"User created with id {user_id}")
+
+    db = next(get_db())
+    print(f"Fetching User...: \n {UserService.get(user_id, db)}")
+
+    db = next(get_db())
+    print("Deleting User...")
+    UserService.delete(user_id, db)
+
+
+    db = next(get_db())
+    if UserService.get(user_id, db) is None:
+        print("Successfully deleted")
+    else:
+        print("Not successful")
