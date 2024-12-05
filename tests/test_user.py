8cf4206f9510f8f6e771ca9268b609830e188d7b
allenmahdi <freshfish0609@gmail.com>
2024-12-05 04:52:14 -0600

    Update test_user.py
    Added test for delete user

--- tests/test_user.py
+++ tests/test_user.py
@@ -20,3 +20,16 @@ def test_get_user(user_access_header, client):
 
     assert response.status_code == 200
     assert response.json()["user_email"] == user_schema.user_email
+
+
+def test_delete_user(client, user_id):
+    """
+    Test deleting a user by their ID.
+    """
+    response = client.delete(f"/users/{user_id}")
+    assert response.status_code == 204
+
+    # Ensure the user is deleted by attempting to retrieve them again
+    response = client.get(f"/users/get-id?email={user_schema.user_email}")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "User not found"
